340d407b3d1fd9822ef8ea644eb901c806cde46b
midCenturyDude7 <mgriffes@gmail.com>
2023-07-20 21:33:42 -0700

    Minor edit

--- app.js
+++ app.js
@@ -94,9 +94,9 @@ export const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
 // console.log(`The sum of all the ages in the array is: ${agesSum}`);
 
 // Get total years for all companies using reduce
-// const totalYears = companies.reduce(
-//   (total, company) => total + (company.end - company.start),
-//   0
-// );
+const totalYears = companies.reduce(
+  (total, company) => total + (company.end - company.start),
+  0
+);
 
-// console.log(`The total years for all companies is: ${totalYears}`);
+console.log(`The total years for all companies is: ${totalYears}`);
